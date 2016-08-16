238f992aa3ef0b58d078fe188a228c43ff40e560
Ricardo Santos <rsantos@nextfaze.com>
2016-08-16 15:06:05 +0930

    More tests for UIViewAdditions

--- Example/Tests/UIViewAdditionsTests.swift
+++ Example/Tests/UIViewAdditionsTests.swift
@@ -39,7 +39,95 @@ class UIViewAdditionsTests: XCTestCase {
         super.tearDown()
     }
     
-    func testSetOrigin() {
+    func testX() {
+        let newX: CGFloat = 50.0
+        view.x = newX
+        
+        XCTAssertEqual(view.origin.x, newX, "origin.x is not as expected")
+        XCTAssertEqual(CGRectGetMinX(view.frame), newX, "minX is not as expected")
+        
+        XCTAssertEqual(view.width, width, "width is not as expected")
+        XCTAssertEqual(view.height, height, "height is not as expected")
+    }
+
+    func testY() {
+        let newY: CGFloat = 50.0
+        view.y = newY
+        
+        XCTAssertEqual(view.origin.y, newY, "origin.y is not as expected")
+        XCTAssertEqual(CGRectGetMinY(view.frame), newY, "minY is not as expected")
+        
+        XCTAssertEqual(view.width, width, "width is not as expected")
+        XCTAssertEqual(view.height, height, "height is not as expected")
+    }
+    
+    func testLeft() {
+        let newLeft: CGFloat = 50.0
+        view.left = newLeft
+        
+        XCTAssertEqual(view.left, newLeft, "left is not as expected")
+        XCTAssertEqual(CGRectGetMinX(view.frame), newLeft, "minX is not as expected")
+        
+        XCTAssertEqual(view.width, width, "width is not as expected")
+        XCTAssertEqual(view.height, height, "height is not as expected")
+    }
+    
+    func testRight() {
+        let newRight: CGFloat = 50.0
+        view.right = newRight
+        
+        XCTAssertEqual(view.right, newRight, "right is not as expected")
+        XCTAssertEqual(CGRectGetMaxX(view.frame), newRight, "maxX is not as expected")
+        
+        XCTAssertEqual(view.width, width, "width is not as expected")
+        XCTAssertEqual(view.height, height, "height is not as expected")
+    }
+    
+    func testTop() {
+        let newTop: CGFloat = 50.0
+        view.top = newTop
+        
+        XCTAssertEqual(view.top, newTop, "top is not as expected")
+        XCTAssertEqual(CGRectGetMinY(view.frame), newTop, "minY is not as expected")
+        
+        XCTAssertEqual(view.width, width, "width is not as expected")
+        XCTAssertEqual(view.height, height, "height is not as expected")
+    }
+    
+    func testBottom() {
+        let newBottom: CGFloat = 50.0
+        view.bottom = newBottom
+        
+        XCTAssertEqual(view.bottom, newBottom, "bottom is not as expected")
+        XCTAssertEqual(CGRectGetMaxY(view.frame), newBottom, "maxY is not as expected")
+        
+        XCTAssertEqual(view.width, width, "width is not as expected")
+        XCTAssertEqual(view.height, height, "height is not as expected")
+    }
+    
+    func testCenterX() {
+        let newCenterX: CGFloat = 50.0
+        view.centerX = newCenterX
+        
+        XCTAssertEqual(view.centerX, newCenterX, "centerX is not as expected")
+        XCTAssertEqual(CGRectGetMidX(view.frame), newCenterX, "midX is not as expected")
+        
+        XCTAssertEqual(view.width, width, "width is not as expected")
+        XCTAssertEqual(view.height, height, "height is not as expected")
+    }
+    
+    func testCenterY() {
+        let newCenterY: CGFloat = 50.0
+        view.centerY = newCenterY
+        
+        XCTAssertEqual(view.centerY, newCenterY, "centerY is not as expected")
+        XCTAssertEqual(CGRectGetMidY(view.frame), newCenterY, "midY is not as expected")
+        
+        XCTAssertEqual(view.width, width, "width is not as expected")
+        XCTAssertEqual(view.height, height, "height is not as expected")
+    }
+    
+    func testOrigin() {
         let newX: CGFloat = 50.0
         let newY: CGFloat = 150.0
         
@@ -57,91 +145,22 @@ class UIViewAdditionsTests: XCTestCase {
         XCTAssertEqual(view.height, height, "height is not as expected")
     }
     
-    func testSetX() {
-        let newX: CGFloat = 50.0
-        view.x = newX
+    func testSize() {
+        let newWidth: CGFloat = 50.0
+        let newHeight: CGFloat = 100.0
         
-        XCTAssertEqual(view.origin.x, newX, "origin.x is not as expected")
-        XCTAssertEqual(CGRectGetMinX(view.frame), newX, "minX is not as expected")
+        view.size = CGSizeMake(newWidth, newHeight)
         
-        XCTAssertEqual(view.width, width, "width is not as expected")
-        XCTAssertEqual(view.height, height, "height is not as expected")
-    }
-
-    func testSetY() {
-        let newY: CGFloat = 50.0
-        view.y = newY
-        
-        XCTAssertEqual(view.origin.y, newY, "origin.y is not as expected")
-        XCTAssertEqual(CGRectGetMinY(view.frame), newY, "minY is not as expected")
-        
-        XCTAssertEqual(view.width, width, "width is not as expected")
-        XCTAssertEqual(view.height, height, "height is not as expected")
+        XCTAssertEqual(view.width, newWidth, "width is not as expected")
+        XCTAssertEqual(view.height, newHeight, "height is not as expected")
     }
     
-    func testSetLeft() {
-        let newLeft: CGFloat = 50.0
-        view.left = newLeft
+    func testAspect() {
+        let newWidth: CGFloat = 50.0
+        let newHeight: CGFloat = 100.0
         
-        XCTAssertEqual(view.left, newLeft, "left is not as expected")
-        XCTAssertEqual(CGRectGetMinX(view.frame), newLeft, "minX is not as expected")
+        view.size = CGSizeMake(newWidth, newHeight)
         
-        XCTAssertEqual(view.width, width, "width is not as expected")
-        XCTAssertEqual(view.height, height, "height is not as expected")
-    }
-    
-    func testSetRight() {
-        let newRight: CGFloat = 50.0
-        view.right = newRight
-        
-        XCTAssertEqual(view.right, newRight, "right is not as expected")
-        XCTAssertEqual(CGRectGetMaxX(view.frame), newRight, "maxX is not as expected")
-        
-        XCTAssertEqual(view.width, width, "width is not as expected")
-        XCTAssertEqual(view.height, height, "height is not as expected")
-    }
-    
-    func testSetTop() {
-        let newTop: CGFloat = 50.0
-        view.top = newTop
-        
-        XCTAssertEqual(view.top, newTop, "top is not as expected")
-        XCTAssertEqual(CGRectGetMinY(view.frame), newTop, "minY is not as expected")
-        
-        XCTAssertEqual(view.width, width, "width is not as expected")
-        XCTAssertEqual(view.height, height, "height is not as expected")
-    }
-    
-    func testSetBottom() {
-        let newBottom: CGFloat = 50.0
-        view.bottom = newBottom
-        
-        XCTAssertEqual(view.bottom, newBottom, "bottom is not as expected")
-        XCTAssertEqual(CGRectGetMaxY(view.frame), newBottom, "maxY is not as expected")
-        
-        XCTAssertEqual(view.width, width, "width is not as expected")
-        XCTAssertEqual(view.height, height, "height is not as expected")
-    }
-    
-    func testSetCenterX() {
-        let newCenterX: CGFloat = 50.0
-        view.centerX = newCenterX
-        
-        XCTAssertEqual(view.centerX, newCenterX, "centerX is not as expected")
-        XCTAssertEqual(CGRectGetMidX(view.frame), newCenterX, "midX is not as expected")
-        
-        XCTAssertEqual(view.width, width, "width is not as expected")
-        XCTAssertEqual(view.height, height, "height is not as expected")
-    }
-    
-    func testSetCenterY() {
-        let newCenterY: CGFloat = 50.0
-        view.centerY = newCenterY
-        
-        XCTAssertEqual(view.centerY, newCenterY, "centerY is not as expected")
-        XCTAssertEqual(CGRectGetMidY(view.frame), newCenterY, "midY is not as expected")
-        
-        XCTAssertEqual(view.width, width, "width is not as expected")
-        XCTAssertEqual(view.height, height, "height is not as expected")
+        XCTAssertEqual(view.aspect, newWidth/newHeight, "aspect is not as expected")
     }
 }
