4645e30f37c1fbdbf4b0d524f7861750ec20396d
Shane Woolcock <swoolcock@nextfaze.com>
2016-11-10 08:36:40 +1030

    Rename NSProcessInfoAdditionsTests in line with Swift 3

--- Example/Tests/ProcessInfoAdditionsTests.swift
+++ Example/Tests/ProcessInfoAdditionsTests.swift
@@ -13,7 +13,7 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 //
-// NSProcessInfoAdditionsTests.swift
+// ProcessInfoAdditionsTests.swift
 // FazeKit
 //
 // Created by swoolcock on 16/08/2016.
@@ -23,7 +23,7 @@ import UIKit
 import XCTest
 import FazeKit
 
-class NSProcessInfoAdditionsTests: XCTestCase {
+class ProcessInfoAdditionsTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
