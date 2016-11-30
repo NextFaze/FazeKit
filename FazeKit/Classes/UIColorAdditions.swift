025d2fb2b219bf1e01b078ea1971fb2702d3f131
Ricardo Santos <rsantos@nextfaze.com>
2016-11-30 15:19:33 +1030

    Add UIColorAdditions with hex string initializer #2

---
+++ FazeKit/Classes/UIColorAdditions.swift
@@ -0,0 +1,48 @@
+//
+// Copyright 2016 NextFaze
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+//
+// UIColorAdditions.swift
+// Pods
+//
+// Created by Ricardo Santos on 30/11/2016.
+//
+//
+
+import UIKit
+
+public extension UIColor {
+    
+    convenience init(redInt: Int, greenInt: Int, blueInt: Int, alpha: CGFloat) {
+        self.init(red: CGFloat(redInt)/255.0, green: CGFloat(greenInt)/255.0, blue: CGFloat(blueInt)/255.0, alpha: alpha)
+    }
+    
+    // adapted from: https://gist.github.com/arshad/de147c42d7b3063ef7bc
+    convenience init(hexString: String, alpha: Double = 1.0) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt32()
+        Scanner(string: hex).scanHexInt32(&int)
+        let r, g, b: UInt32
+        switch hex.characters.count {
+        case 3: // RGB (12-bit)
+            (r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (r, g, b) = (1, 1, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(255 * alpha) / 255)
+    }
+
+}
