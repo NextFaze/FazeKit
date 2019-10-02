59351f23f2265053392b8de70bda873c0dbb192a
Shane Woolcock <swoolcock@nextfaze.com>
2019-10-03 08:51:19 +0930

    Add Data functions

--- FazeKit/Classes/CollectionAdditions.swift
+++ FazeKit/Classes/CollectionAdditions.swift
@@ -44,7 +44,7 @@ public extension Collection {
 public extension Sequence {
     /// Categorises elements of self into a dictionary, with the keys given by keyFunc
     func categorise<U: Hashable>(_ keyFunc: (Iterator.Element) -> U) -> [U: [Iterator.Element]] {
-        var dict: [U:[Iterator.Element]] = [:]
+        var dict: [U: [Iterator.Element]] = [:]
         for el in self {
             let key = keyFunc(el)
             if case nil = dict[key]?.append(el) { dict[key] = [el] }
