98ff5d3f800220508a5a1f1f9f057daf09e41b11
Shane Woolcock <swoolcock@nextfaze.com>
2019-10-03 11:40:00 +0930

    Revert "Remove unwanted things from the example project"
    This reverts commit ab2f3ac84f4934fbc69888a6e69990c8e1a2c635.

---
+++ Example/Pods/Target Support Files/Pods-FazeKit_Tests/Pods-FazeKit_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FazeKit_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FazeKit_TestsVersionString[];
+
