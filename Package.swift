b6531d4a150f35f4d3218664e7d6d4f930d97dae
Michael Drozdowski <michael.drozdowski@gmail.com>
2019-10-31 14:26:35 +1030

    Update package.swift syntx

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "FazeKit",
     platforms: [
-        .iOS(.v10)
-    ]
+        .iOS(.v10),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
