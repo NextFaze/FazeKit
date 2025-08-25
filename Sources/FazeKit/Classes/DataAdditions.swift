2cf6f3b956394e520f88b9d91d8263c2b850709e
Ric Santos <rics@ntos.me>
2025-08-25 12:35:37 +0930

    Add base64 url safe encoding/decoding extension for Data

--- Sources/FazeKit/Classes/DataAdditions.swift
+++ Sources/FazeKit/Classes/DataAdditions.swift
@@ -27,3 +27,34 @@ public extension Data {
         return String(format: str as String)
     }
 }
+
+public extension Data {
+    /// Returns a Base-64 URL-safe encoded string.
+    ///
+    /// - parameter options: The options to use for the encoding. Default value is `[]`.
+    /// - returns: The Base-64 URL-safe encoded string.
+    func base64URLEncodedString(options: Data.Base64EncodingOptions = []) -> String {
+        base64EncodedString()
+            .replacingOccurrences(of: "/", with: "_")
+            .replacingOccurrences(of: "+", with: "-")
+            .replacingOccurrences(of: "=", with: "")
+    }
+    
+    /// Initialize a `Data` from a Base-64 URL encoded String using the given options.
+    ///
+    /// Returns nil when the input is not recognized as valid Base-64.
+    /// - parameter base64URLString: The string to parse.
+    /// - parameter options: Encoding options. Default value is `[]`.
+    init?(base64URLEncoded base64URLString: String, options: Data.Base64DecodingOptions = []) {
+        var base64Encoded = base64URLString
+            .replacingOccurrences(of: "_", with: "/")
+            .replacingOccurrences(of: "-", with: "+")
+        
+        // Pad the string with "=" signs until the string's length is a multiple of 4.
+        while !base64Encoded.count.isMultiple(of: 4) {
+            base64Encoded.append("=")
+        }
+        
+        self.init(base64Encoded: base64Encoded, options: options)
+    }
+}
